7ff159468356f510c4bf020e1453c4ad396ed096
Mehedi Hasan <mehediofficials28@gmail.com>
2026-08-12 19:02:21 +0600

    Added

--- backend/apps/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from apps.core.views import PublicSiteConfigView, DashboardSummaryView
-
-app_name = 'core'
-
-urlpatterns = [
-    path('settings/', PublicSiteConfigView.as_view(), name='site-settings'),
-    path('dashboard/summary/', DashboardSummaryView.as_view(), name='dashboard-summary'),
-]
